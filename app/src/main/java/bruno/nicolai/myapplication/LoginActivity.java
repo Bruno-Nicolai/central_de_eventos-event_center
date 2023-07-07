e616184976348203604877c9bfd7faef2eaf743e
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2023-07-07 09:36:28 -0300

    LoginActivity Remake

--- app/src/main/java/bruno/nicolai/myapplication/LoginActivity.java
+++ app/src/main/java/bruno/nicolai/myapplication/LoginActivity.java
@@ -1,16 +1,17 @@
 package bruno.nicolai.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 import bruno.nicolai.myapplication.databinding.ActivityLoginBinding;
 
 
 public class LoginActivity extends AppCompatActivity {
-
 
     private ActivityLoginBinding binding;
 
@@ -20,22 +21,50 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.vvHeader.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.login_bg)
+        );
+        binding.vvHeader.start();
+        binding.vvHeader.setOnPreparedListener(mediaPlayer -> {
+            mediaPlayer.setLooping(true);
+        });
+
         binding.loginBtnNext.setOnClickListener(view -> {
             login();
         });
     }
 
     public void login() {
-        String user = binding.loginEtEmail.getText().toString().trim();
-        String password = binding.loginEtPassword.getText().toString().trim();
-
-        if (user.equals("admin") && password.equals("admin")) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Try again.", Toast.LENGTH_LONG).show();
-        }
-
+//        String user = binding.loginEtEmail.getText().toString().trim();
+        goToMainActivity();
     }
 
+    public void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        binding.vvHeader.resume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        binding.vvHeader.start();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        binding.vvHeader.suspend();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        binding.vvHeader.stopPlayback();
+        super.onDestroy();
+    }
 }
